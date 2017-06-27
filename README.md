9b77fbbf7e0fde32182ca727df9a570dc77d77e6
ilammy <a.lozovsky@gmail.com>
2017-06-28 01:16:27 +0300

    Basic project

---
+++ README.md
@@ -0,0 +1,9 @@
+Rapier
+======
+
+**Rapier** is a GUI wrapper for OpenVPN.
+
+Licensing
+---------
+
+The code is licensed under [**MIT License**](LICENSE).
---
+++ Rapier/AppDelegate.swift
@@ -0,0 +1,11 @@
+// Copyright (c) 2017, ilammy
+//
+// Licensed under MIT license (see LICENSE in the root directory). This file
+// may be copied, distributed, and modified only in accordance with the terms
+// specified by the chosen license.
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+}
