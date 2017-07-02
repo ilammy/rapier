d43ab66bb884fce6668076394583ed1d195e7252
ilammy <a.lozovsky@gmail.com>
2017-07-02 12:46:49 +0300

    Add configurations menu

--- Rapier/StatusMenuController.swift
+++ Rapier/StatusMenuController.swift
@@ -14,9 +14,42 @@ class StatusMenuController: NSObject {
     override func awakeFromNib() {
         statusItem.title = "Rapier"
         statusItem.menu = menu
+
+        addConfigurations()
     }
 
     @IBAction func quitClicked(_ sender: NSMenuItem) {
         NSApplication.shared().terminate(self)
+    }
+
+    private func addConfigurations() {
+        let configurations = [("Default", "~/Documents/openvpn.conf")]; // TOOD: hardcode
+
+        for (i, (confName, confPath)) in configurations.enumerated() {
+            menu.insertItem(configurationItem(confName, confPath), at: i)
+        }
+
+        if configurations.count > 0 {
+            menu.insertItem(NSMenuItem.separator(), at: configurations.count)
+        }
+    }
+
+    private func configurationItem(_ name: String, _ path: String) -> NSMenuItem {
+        let item = NSMenuItem()
+
+        item.title = name
+        item.keyEquivalent = path
+        item.toolTip = path
+        item.target = self
+        item.action = #selector(StatusMenuController.configurationClicked(_:))
+        item.state = 0
+
+        return item
+    }
+
+    @objc private func configurationClicked(_ sender: NSMenuItem) {
+        NSLog("configuration selected: \(sender.keyEquivalent)")
+
+        sender.state = (sender.state != 0) ? 0 : 1;
     }
 }
